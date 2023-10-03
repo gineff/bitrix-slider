dce9cf1fedcae2ed010611c066f4dd16699a0015
gineff <canone@inbox.ru>
2023-10-03 15:00:15 +0300

    chore: add initial settings

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  rollupOptions: {
+    output: {
+      dir: 'dist',
+      entryFileNames: 'script.js',
+      chunkFileNames: 'script.js',
+      assetFileNames: 'style[extname]',
+    },
+  },
 })
