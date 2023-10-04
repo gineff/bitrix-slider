f44774bcc62675a7b32db904549b5c66bbaefd67
gineff <canone@inbox.ru>
2023-10-04 17:47:05 +0300

    ft: edit vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './src/main.jsx',
+      },
+      output: {
+        dir: 'dist',
+        entryFileNames: 'script.js',
+        chunkFileNames: 'script.js',
+        assetFileNames: 'style[extname]',
+      },
+    },
+  },
 })
